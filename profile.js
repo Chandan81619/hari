8f93745206cf4a34f98741798ec52a2d8f14182d
Chandan81619 <chandan81619@gmail.com>
2025-06-22 15:05:55 +0545

    upadate

---
+++ profile.js
@@ -0,0 +1,10 @@
+// Smooth scroll for nav links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    document.getElementById(targetId).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
